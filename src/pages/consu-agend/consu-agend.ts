dd4169c309d72e1ecf6e6594bcf0924a0cd8071d
Teles <macos@Teles.local>
2019-06-26 20:10:21 -0300

    m,nasdbfabmsdfjkad

--- src/pages/consu-agend/consu-agend.ts
+++ src/pages/consu-agend/consu-agend.ts
@@ -16,48 +16,87 @@ export interface Cliente {
 export class ConsuAgendPage {
   agendId: any;
   object = Object.keys;
-  cliente = {} as Cliente;
-  salao: any;
+  // saloes:any;
+  saloes:any;
   constructor(public navCtrl: NavController, public navParams: NavParams, private db: AngularFireDatabase) {
-   this.getUser();
+  //  this.getUser();
   }
 
 
-  getUser(){
-    this.db.object('usuarios/').snapshotChanges().subscribe((user)=>{
-      this.agendId = user.payload.val()
-      var object: any[] = this.object(this.agendId);
-      object.map((dado) =>{
-       this.getAgend(dado);
-       console.log(dado)
-      });
-    })
-  }
+  ngOnInit(){
+    this.db.object('usuarios/'+localStorage.getItem("uid") + "/info/agendamentos").snapshotChanges().subscribe((agendamentos)=>{
+        let agend = [];
 
-  getAgend(dado){
-    this.db.object('agendamentos/'+dado).snapshotChanges().subscribe((agend)=>{
-      var aux = agend.payload.val()
-      if(aux != null){
-      var object: any[] = this.object(aux);
-      object.map((data) =>{
-       console.log(data)
-       this.getAll(dado, data);
-      });
-    }
-    })
-  }
-
-  getAll(dado, data){
-    this.db.object('agendamentos/'+dado+"/"+data).snapshotChanges().subscribe((val)=>{
-      if(localStorage.getItem('uid') == val.payload.val().cliente){
-        this.cliente.data = val.payload.val().data;
-        this.cliente.horario = val.payload.val().horario;
-
-        this.db.object('usuarios/'+dado+"/info/salao").snapshotChanges().subscribe((salon)=>{
-          this.salao = salon.payload.val().nome;
+        Object.keys(agendamentos.payload.val()).map((x)=>{
+          return {agendamento:agendamentos.payload.val()[x],key:x};
         })
-      }
+        .forEach((e)=>{
+          this.db.object("agendamentos").snapshotChanges().subscribe((agendamentos)=>{
+              Object.keys(agendamentos.payload.val()).map((x)=>{
+                  Object.keys(agendamentos.payload.val()[x]).map((j)=>{
+                    if(e.agendamento == j){
+                      agend.push(agendamentos.payload.val()[x][j]);
+                    }
+                  })
+                  
+                  // return;
+              })
+              
+              this.saloes = agend
+
+              this.saloes.forEach((e,index)=>{
+                this.db.object("usuarios/"+e.salao+"/info/salao").snapshotChanges().subscribe((salao)=>{
+
+                    this.saloes[index]["nome"]=salao.payload.val().nome
+                })
+              })
+              console.log(this.saloes);
+              
+
+          })
+        })
+
+        
     })
-}
+  }
+
+
+
+//   getUser(){
+//     this.db.object('usuarios/').snapshotChanges().subscribe((user)=>{
+//       this.agendId = user.payload.val()
+//       var object: any[] = this.object(this.agendId);
+//       object.map((dado) =>{
+//        this.getAgend(dado);
+//        console.log(dado)
+//       });
+//     })
+//   }
+
+//   getAgend(dado){
+//     this.db.object('agendamentos/'+dado).snapshotChanges().subscribe((agend)=>{
+//       var aux = agend.payload.val()
+//       if(aux != null){
+//       var object: any[] = this.object(aux);
+//       object.map((data) =>{
+//        console.log(data)
+//        this.getAll(dado, data);
+//       });
+//     }
+//     })
+//   }
+
+//   getAll(dado, data){
+//     this.db.object('agendamentos/'+dado+"/"+data).snapshotChanges().subscribe((val)=>{
+//       if(localStorage.getItem('uid') == val.payload.val().cliente){
+//         this.cliente.data = val.payload.val().data;
+//         this.cliente.horario = val.payload.val().horario;
+
+//         this.db.object('usuarios/'+dado+"/info/salao").snapshotChanges().subscribe((salon)=>{
+//           this.salao = salon.payload.val().nome;
+//         })
+//       }
+//     })
+// }
 
 }
